a499eb64094da38964b430cad7085c272f445aac
Deepu <dpnr915@gmail.com>
2026-01-28 22:54:57 +0530

    minor modifications before starting the development

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import { Box } from '@mui/material'
+
+const App = () => {
+  console.log("Render");
+  return (
+    <>
+
+      Perfume
+
+      Gabrielle Essence Eau De Parfum
+
+      A floral, solar and voluptuous interpretation composed by Olivier Polge,
+      Perfumer-Creator for the House of CHANEL.
+
+      $149.99
+      $169.99
+
+      Add to Cart
+
+      <div >
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Deepu</a>.
+      </div>
+
+    </>
+  )
+}
+
+export default App
